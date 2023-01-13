4c916c912bccc66de484c28f12ea4a1bd5022245
kala-nea <116219429+kala-nea@users.noreply.github.com>
2023-01-13 07:32:20 -0800

    started 1st page
    started content/styling on 1st page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,8 +1,30 @@
-<script>
+<!-- JS CODE -->
 
-</script>
+<!-- HTML CODE -->
 
-<h1>Stable Scout 2</h1>
-<a href="">Upload</a>
-<a href="/scoutPart1">Scout</a>
+<!-- Section for flexbox purposes -->
+<section>
+    <!-- Title of App -->
+    <h1>Stable Scout 2</h1>
 
+    <!-- Links for starting scouting and uploading data -->
+    <a href="">Upload</a>
+    <a href="/scoutPart1">Scout</a>
+</section>
+
+
+<!-- CSS CODE -->
+<style>
+    /* Applying columned layout to page */
+    section {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+    }
+
+    /* Styling to make links look like buttons */
+    a {
+        padding: 10px;
+        background-color: gray;
+    }
+</style>
