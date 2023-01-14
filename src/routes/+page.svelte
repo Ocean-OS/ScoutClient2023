a562c7dc2aaee1e2a8ddbae9e1b1262f7ccd5ff5
kala-nea <116219429+kala-nea@users.noreply.github.com>
2023-01-13 21:14:57 -0800

    styling
    did styling for 1st and 2nd pages

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -8,8 +8,8 @@
     <h1>Stable Scout 2</h1>
 
     <!-- Links for starting scouting and uploading data -->
-    <a href="">Upload</a>
     <a href="/scoutPart1">Scout</a>
+    <a href="">Upload</a>
 </section>
 
 
@@ -17,15 +17,37 @@
 <style>
     /* Applying columned layout to page */
     section {
-        width: 100vw;
+        width: 100%;
+        height: 98vh;
         display: flex;
         flex-direction: column;
         align-items: center;
+        justify-content: center;
+
+        background-color: #30ace2;
     }
 
     /* Styling to make links look like buttons */
     a {
+        width: 50%;
         padding: 10px;
-        background-color: gray;
+        padding-top: 30px;
+        padding-bottom: 30px;
+        margin: 10px;
+
+        background-color: black;
+        color: #d8d8d8;
+        text-decoration: none;
+        text-align: center;
+        font-size: 50px;
+    }
+
+    h1 {
+        margin-bottom: 25%;
+        width: 50%;
+
+        color: #d8d8d8;
+        font-size: 60px;
+        text-align: center;
     }
 </style>
--- src/routes/scoutPart1/+page.svelte
+++ src/routes/scoutPart1/+page.svelte
@@ -2,25 +2,29 @@
 
 <!-- HTML CODE -->
 
-<!-- Back button just in case user accidentally presses scout button -->
-<a href="/">Back</a>
-
 <!-- Section for flexbox purposes -->
 <section>
+    <!-- Back button just in case user accidentally presses scout button -->
+    <a href="/" class="backButton">Back</a>
+
     <!-- Page Title -->
     <h1>Pregame info</h1>
 
-    <!-- Input area for team name -->
-    <p>Team: {}</p>
-    <input />
+    <!-- Section to group input areas together for margin purposes -->
+    <section class="inputArea">
+        <!-- Input area for team name -->
+        <p>Team #:</p>
+        <input />
 
-    <!-- Input area for round # -->
-    <p>Round: {}</p>
-    <input />
+        <!-- Input area for round # -->
+        <p>Round #:</p>
+        <input />
 
-    <!-- Input area for alliance color -->
-    <p>Alliance: {}</p>
-    <input />
+        <!-- Input area for alliance color -->
+        <p>Alliance:</p>
+        <input />
+    </section>
+    
 
     <!-- Link to scoring page -->
     <a href="/scoutPart2">To Scoring</a>
@@ -29,13 +33,59 @@
 
 <!-- CSS CODE -->
 <style>
-    /* Centering items */
+    /* Applying columned layout to page */
     section {
-        width: 100vw;
+        width: 100%;
+        height: 98vh;
         display: flex;
         flex-direction: column;
         align-items: center;
+        justify-content: center;
+
+        background-color: #30ace2;
     }
 
-    
+    /* Styling to make links look like buttons */
+    a {
+        width: 50%;
+        padding: 10px;
+        padding-top: 30px;
+        padding-bottom: 30px;
+        margin: 10px;
+
+        background-color: black;
+        color: #d8d8d8;
+        text-decoration: none;
+        text-align: center;
+        font-size: 50px;
+    }
+
+    h1 {
+        margin-bottom: 0px;
+        width: 80%;
+
+        color: #d8d8d8;
+        font-size: 60px;
+        text-align: center;
+    }
+
+    p {
+        color: #d8d8d8;
+        font-size: 30px;
+        margin-bottom: 0px;
+    }
+
+    .backButton {
+        width: 20%;
+        padding: 10px;
+        margin: 10px;
+
+        font-size: 25px;
+
+        align-self: flex-start;
+    }
+
+    .inputAreas {
+        margin-bottom: 15%;
+    }
 </style>
