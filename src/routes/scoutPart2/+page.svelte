d5322a7028ae69dfd634102a14f7c1a45d790a37
kala-nea <116219429+kala-nea@users.noreply.github.com>
2023-01-16 09:55:41 -0800

    cone counter done
    cone counter done

--- src/routes/scoutPart2/+page.svelte
+++ src/routes/scoutPart2/+page.svelte
@@ -1,7 +1,210 @@
 <!-- JS CODE -->
+<script>
+	import { append, set_attributes } from "svelte/internal";
+
+
+    // Define variables
+    let coneNum = 0;
+    let cubeNum = 0;
+    let totalNum = 0;
+    let att = 0;
+    let fouls = 0;
+    let tFouls = 0;
+    let c1 = 0;
+    let c2 = 0;
+    let c3 = 0;
+    let c4 = 0;
+    let c5 = 0;
+    let c6 = 0;
+    let c7 = 0;
+    let c8 = 0;
+    let c9 = 0;
+    let c10 = 0;
+    let c11 = 0;
+    let c12 = 0;
+    let b1 = 0;
+    let b2 = 0;
+    let b3 = 0;
+    let b4 = 0;
+    let b5 = 0;
+    let b6 = 0;
+    let b7 = 0;
+    let b8 = 0;
+    let b9 = 0;
+    let b10 = 0;
+    let b11 = 0;
+    let b12 = 0;
+    let h1 = 0;
+    let h2 = 0;
+    let h3 = 0;
+    let h4 = 0;
+    let h5 = 0;
+    let h6 = 0;
+    let h7 = 0;
+    let h8 = 0;
+    let h9 = 0;
+
+    // attempted nodes functions
+    function attIncr () {
+        att = att + 1;
+        return(att);
+    }
+
+    function attDecr () {
+        if (att > 0) {
+            att = att - 1;
+        }
+    }
+
+    // fouls functions
+    function foulsIncr () {
+        fouls = fouls + 1;
+    }
+
+    function foulsDecr () {
+        if (fouls > 0) {
+            fouls = fouls - 1;
+        }
+    }
+
+    // tech fouls functions
+    function tFoulsIncr () {
+        tFouls = tFouls + 1;
+    }
+
+    function tFoulsDecr () {
+        if (tFouls > 0) {
+            tFouls = tFouls - 1;
+        }
+    }
+
+    // cone checkbox functions
+    function coneCheck () {
+        
+    }
+
+    function c1Check () {
+        if (c1 == 0) {
+            c1 = 1;
+        } else {
+            c1 = 0;
+        }
+        coneNum = c1 + c2 + c3 + c4 + c5 + c6 + c7 + c8 + c9 + c10 + c11 + c12;
+        totalNum = coneNum + cubeNum + h1 + h2 + h3 + h4 + h5 + h6 + h7 + h8 + h9;
+    }
+
+    function c2Check () {
+        if (c2 == 0) {
+            c2 = 1;
+        } else {
+            c2 = 0;
+        }
+        coneNum = c1 + c2 + c3 + c4 + c5 + c6 + c7 + c8 + c9 + c10 + c11 + c12;
+        totalNum = coneNum + cubeNum + h1 + h2 + h3 + h4 + h5 + h6 + h7 + h8 + h9;
+    }
+
+    function c3Check () {
+        if (c3 == 0) {
+            c3 = 1;
+        } else {
+            c3 = 0;
+        }
+        coneNum = c1 + c2 + c3 + c4 + c5 + c6 + c7 + c8 + c9 + c10 + c11 + c12;
+        totalNum = coneNum + cubeNum + h1 + h2 + h3 + h4 + h5 + h6 + h7 + h8 + h9;
+    }
+
+    function c4Check () {
+        if (c4 == 0) {
+            c4 = 1;
+        } else {
+            c4 = 0;
+        }
+        coneNum = c1 + c2 + c3 + c4 + c5 + c6 + c7 + c8 + c9 + c10 + c11 + c12;
+        totalNum = coneNum + cubeNum + h1 + h2 + h3 + h4 + h5 + h6 + h7 + h8 + h9;
+    }
+
+    function c5Check () {
+        if (c5 == 0) {
+            c5 = 1;
+        } else {
+            c5 = 0;
+        }
+        coneNum = c1 + c2 + c3 + c4 + c5 + c6 + c7 + c8 + c9 + c10 + c11 + c12;
+        totalNum = coneNum + cubeNum + h1 + h2 + h3 + h4 + h5 + h6 + h7 + h8 + h9;
+    }
+
+    function c6Check () {
+        if (c6 == 0) {
+            c6 = 1;
+        } else {
+            c6 = 0;
+        }
+        coneNum = c1 + c2 + c3 + c4 + c5 + c6 + c7 + c8 + c9 + c10 + c11 + c12;
+        totalNum = coneNum + cubeNum + h1 + h2 + h3 + h4 + h5 + h6 + h7 + h8 + h9;
+    }
+
+    function c7Check () {
+        if (c7 == 0) {
+            c7 = 1;
+        } else {
+            c7 = 0;
+        }
+        coneNum = c1 + c2 + c3 + c4 + c5 + c6 + c7 + c8 + c9 + c10 + c11 + c12;
+        totalNum = coneNum + cubeNum + h1 + h2 + h3 + h4 + h5 + h6 + h7 + h8 + h9;
+    }
+
+    function c8Check () {
+        if (c8 == 0) {
+            c8 = 1;
+        } else {
+            c8 = 0;
+        }
+        coneNum = c1 + c2 + c3 + c4 + c5 + c6 + c7 + c8 + c9 + c10 + c11 + c12;
+        totalNum = coneNum + cubeNum + h1 + h2 + h3 + h4 + h5 + h6 + h7 + h8 + h9;
+    }
+
+    function c9Check () {
+        if (c9 == 0) {
+            c9 = 1;
+        } else {
+            c9 = 0;
+        }
+        coneNum = c1 + c2 + c3 + c4 + c5 + c6 + c7 + c8 + c9 + c10 + c11 + c12;
+        totalNum = coneNum + cubeNum + h1 + h2 + h3 + h4 + h5 + h6 + h7 + h8 + h9;
+    }
+
+    function c10Check () {
+        if (c10 == 0) {
+            c10 = 1;
+        } else {
+            c10 = 0;
+        }
+        coneNum = c1 + c2 + c3 + c4 + c5 + c6 + c7 + c8 + c9 + c10 + c11 + c12;
+        totalNum = coneNum + cubeNum + h1 + h2 + h3 + h4 + h5 + h6 + h7 + h8 + h9;
+    }
+
+    function c11Check () {
+        if (c11 == 0) {
+            c11 = 1;
+        } else {
+            c11 = 0;
+        }
+        coneNum = c1 + c2 + c3 + c4 + c5 + c6 + c7 + c8 + c9 + c10 + c11 + c12;
+        totalNum = coneNum + cubeNum + h1 + h2 + h3 + h4 + h5 + h6 + h7 + h8 + h9;
+    }
+
+    function c12Check () {
+        if (c12 == 0) {
+            c12 = 1;
+        } else {
+            c12 = 0;
+        }
+        coneNum = c1 + c2 + c3 + c4 + c5 + c6 + c7 + c8 + c9 + c10 + c11 + c12;
+        totalNum = coneNum + cubeNum + h1 + h2 + h3 + h4 + h5 + h6 + h7 + h8 + h9;
+    }
+</script>
 
 <!-- HTML CODE -->
-
 <!-- Section for flexbox purposes -->
 <section>
     <!-- Back button just in case user accidentally presses scout button -->
@@ -16,116 +219,118 @@
     <!-- Autonomous criteria grid -->
     <section class="scoutingArea">
 
+        <p>Cones: {coneNum}</p>
+
         <!-- Interactable node grid area -->
         <!-- Outline for INGA -->
         <section class="INGA">
             <!-- Top row of INGA -->
             <div class="yellow">
-                <input type="checkbox" class="cone">
+                <button class="cone" on:click={c1Check}></button>
             </div>
             <div class="purple">
-                <input type="checkbox" class="box">
+                <button class="box"></button>
             </div>
             <div class="yellow">
-                <input type="checkbox" class="cone">
+                <button class="cone" on:click={c2Check}></button>
             </div>
             <div class="yellow">
-                <input type="checkbox" class="cone">
+                <button class="cone" on:click={c3Check}></button>
             </div>
             <div class="purple">
-                <input type="checkbox" class="box">
+                <button class="box"></button>
             </div>
             <div class="yellow">
-                <input type="checkbox" class="cone">
+                <button class="cone" on:click={c4Check}></button>
             </div>
             <div class="yellow">
-                <input type="checkbox" class="cone">
+                <button class="cone" on:click={c5Check}></button>
             </div>
             <div class="purple">
-                <input type="checkbox" class="box">
+                <button class="box"></button>
             </div>
             <div class="yellow">
-                <input type="checkbox" class="cone">
+                <button class="cone" on:click={c6Check}></button>
             </div>
 
             <!-- Mid row of INGA -->
             <div class="yellow">
-                <input type="checkbox" class="cone">
+                <button class="cone" on:click={c7Check}></button>
             </div>
             <div class="purple">
-                <input type="checkbox" class="box">
+                <button class="box"></button>
             </div>
             <div class="yellow">
-                <input type="checkbox" class="cone">
+                <button class="cone" on:click={c8Check}></button>
             </div>
             <div class="yellow">
-                <input type="checkbox" class="cone">
+                <button class="cone" on:click={c9Check}></button>
             </div>
             <div class="purple">
-                <input type="checkbox" class="box">
+                <button class="box"></button>
             </div>
             <div class="yellow">
-                <input type="checkbox" class="cone">
+                <button class="cone" on:click={c10Check}></button>
             </div>
             <div class="yellow">
-                <input type="checkbox" class="cone">
+                <button class="cone" on:click={c11Check}></button>
             </div>
             <div class="purple">
-                <input type="checkbox" class="box">
+                <button class="box"></button>
             </div>
             <div class="yellow">
-                <input type="checkbox" class="cone">
+                <button class="cone" on:click={c12Check}></button>
             </div>
 
             <!-- Low row of INGA -->
             <div class="dual">
-                <input type="checkbox" class="hybrid">
+                <button class="hybrid"></button>
             </div>
             <div class="dual">
-                <input type="checkbox" class="hybrid">
+                <button class="hybrid"></button>
             </div>
             <div class="dual">
-                <input type="checkbox" class="hybrid">
+                <button class="hybrid"></button>
             </div>
             <div class="dual">
-                <input type="checkbox" class="hybrid">
+                <button class="hybrid"></button>
             </div>
             <div class="dual">
-                <input type="checkbox" class="hybrid">
+                <button class="hybrid"></button>
             </div>
             <div class="dual">
-                <input type="checkbox" class="hybrid">
+                <button class="hybrid"></button>
             </div>
             <div class="dual">
-                <input type="checkbox" class="hybrid">
+                <button class="hybrid"></button>
             </div>
             <div class="dual">
-                <input type="checkbox" class="hybrid">
+                <button class="hybrid"></button>
             </div>
             <div class="dual">
-                <input type="checkbox" class="hybrid">
+                <button class="hybrid"></button>
             </div>
         </section>
 
         <!-- Input area for attempted nodes -->
         <section class="inputArea">
-            <p>Attempted: 0</p>
-            <button class="Incr">+</button>
-            <button class="Decr">-</button>
+            <p>Attempted: {att}</p>
+            <button class="Incr" on:click={attIncr}>+</button>
+            <button class="Decr" on:click={attDecr}>-</button>
         </section>
         
         <!-- Input area for tech fouls -->
         <section class="inputArea">
-            <p>Tech Fouls: 0</p>
-            <button class="Incr">+</button>
-            <button class="Decr">-</button>
+            <p>Tech Fouls: {tFouls}</p>
+            <button class="Incr" on:click={tFoulsIncr}>+</button>
+            <button class="Decr" on:click={tFoulsDecr}>-</button>
         </section>
         
         <!-- Input area for fouls -->
         <section class="inputArea">
-            <p>Fouls: 0</p>
-            <button class="Incr">+</button>
-            <button class="Decr">-</button>
+            <p>Fouls: {fouls}</p>
+            <button class="Incr" on:click={foulsIncr}>+</button>
+            <button class="Decr" on:click={foulsDecr}>-</button>
         </section>
         
         <!-- Input area for charge station -->
@@ -238,23 +443,23 @@
 
         <!-- Input area for attempted nodes -->
         <section class="inputArea">
-            <p>Attempted: 0</p>
-            <button class="Incr">+</button>
-            <button class="Decr">-</button>
+            <p>Attempted: {att}</p>
+            <button class="Incr" on:click={attIncr}>+</button>
+            <button class="Decr" on:click={attDecr}>-</button>
         </section>
         
         <!-- Input area for tech fouls -->
         <section class="inputArea">
-            <p>Tech Fouls: 0</p>
-            <button class="Incr">+</button>
-            <button class="Decr">-</button>
+            <p>Tech Fouls: {tFouls}</p>
+            <button class="Incr" on:click={tFoulsIncr}>+</button>
+            <button class="Decr" on:click={tFoulsDecr}>-</button>
         </section>
         
         <!-- Input area for fouls -->
         <section class="inputArea">
-            <p>Fouls: 0</p>
-            <button class="Incr">+</button>
-            <button class="Decr">-</button>
+            <p>Fouls: {fouls}</p>
+            <button class="Incr" on:click={foulsIncr}>+</button>
+            <button class="Decr" on:click={foulsDecr}>-</button>
         </section>
     </section>
 
@@ -264,13 +469,14 @@
     <!-- Endgame criteria grid -->
     <section class="scoutingArea">
         
-        <!-- Input area for charge station -->
+        <!-- Input area for ending position -->
         <section class="inputArea">
-            <p>Charge Station:</p>
+            <p>Ending Position:</p>
             <select name="chargeStation" id="chargeStation">
-                <option value="0">Off</option>
-                <option value="1">Docked</option>
-                <option value="2">Engaged</option>
+                <option value="0">Field</option>
+                <option value="1">Community</option>
+                <option value="2">Docked</option>
+                <option value="3">Engaged</option>
             </select>
         </section>
     </section>
@@ -357,20 +563,34 @@
         flex-direction: row;
         flex-wrap: wrap;
         justify-content: space-between;
+        align-content: center;
         margin-bottom: 20px;
-        background-color: #ffff00;
+        background: linear-gradient(90deg, rgba(0,0,255,1) 33.75%, rgba(216,216,216,1) 33.75%, rgba(216,216,216,1) 66.25%, rgba(0,0,255,1) 66.25%);
         border: solid 1px black;
+        width: 100%;
+        height: 100px;
+        padding: 5px;
     }
 
     .cone,
     .box,
     .hybrid {
-        width: 29px;
-        height: 29px;
+        width: 80%;
+        height: 80%;
+        margin: 5px;
+        padding: 0px;
+    }
+
+    div {
+        width: 10%;
+        height: 33%;
+        display: flex;
+        justify-content: center;
+        align-items: center;
     }
 
     .cone {
-        clip-path: circle(46% at 50% 50%);
+        border-radius: 50%;
     }
 
     .inputArea {
@@ -381,8 +601,8 @@
 
     .Incr,
     .Decr {
-        width: 20px;
-        height: 20px;
+        width: 25px;
+        height: 25px;
         margin-left: 5px;
     }
 
--- src/routes/upload/+page.svelte
+++ src/routes/upload/+page.svelte
@@ -9,7 +9,7 @@
     <h1>Upload Data</h1>
 
     <!-- Links for starting scouting and uploading data -->
-    <a href="/routes">To Home</a>
+    <a href="/">To Home</a>
 </section>
 
 <!-- CSS CODE -->
