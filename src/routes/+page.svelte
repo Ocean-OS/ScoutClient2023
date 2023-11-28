5ce852a30d618728bb73d493fab73a3799090308
ComputerGuy <63362464+Ocean-OS@users.noreply.github.com>
2023-11-27 21:13:08 -0800

    Added Discord Webhook stuff, edit line 9 before compiling

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,7 +3,48 @@
     import Papa from "papaparse";
     import { db } from "$lib/db";
 
-    function saveData() {
+    function sendData() {
+        db.scoutData.toArray().then((data) => {
+        let csv = Papa.unparse(data);
+        var webhookURL = "insert Discord Webhook URL here";
+        // Create a blob for the CSV file
+        const blob = new Blob([csv], { type: 'text/csv;charset=utf-8;' });
+        try{
+            var scouter = Papa.parse(csv).data[1][0]; //I'm not fully sure if this will work
+        }catch(err){
+            var scouter = undefined;
+        }
+        if(scouter == ""){
+            scouter = undefined;
+        }
+        var fd = new FormData(); //Build message for webhook
+        if(scouter !== undefined){ 
+            fd.append("content", `${scouter} posted some new data: `);
+        }
+        fd.append("file", blob, "output.csv"); //Attach file as blob to message
+        fetch(webhookURL, {
+            method: 'POST',
+            body: fd
+        }).then(response => console.log(response))
+        .catch(error =>{ 
+            //If an error occurs, file will download instead
+            console.error(error);
+            const url = URL.createObjectURL(blob);
+            const link = document.createElement("a");
+            link.setAttribute("href", url);
+            link.setAttribute("download", "output.csv");
+            link.style.display = "none";
+            document.body.appendChild(link);
+            link.click();
+            document.body.removeChild(link);
+            URL.revokeObjectURL(url);
+        });
+
+        // Clear database
+        db.scoutData.clear();
+})
+    }
+ function saveData() {
         db.scoutData.toArray().then((data) => {
         let csv = Papa.unparse(data);
 
@@ -45,6 +86,7 @@
             <div class="text-blue-900 text-4xl justify-center flex flex-col flex-grow items-center mb-52">
                 <a class="bg-blue-400 rounded-lg px-32 p-4 m-2" href="/scoutPart1">Scout</a>
                 <button class="bg-blue-400 rounded-lg px-24 p-4" on:click={saveData}>Save Data</button>
+                <button class="bg-blue-400 rounded-lg px-24 p-4" on:click={sendData}>Send Data</button>
             </div>
         </div>
     </div>
